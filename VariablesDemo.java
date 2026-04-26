35ead4bc1336ee3b8e81e9659e85642d7e9b1682
LINUS LWEKAZA <linusdonatus777@gmail.com>
2026-04-26 23:02:20 +0300

    Added VariablesDemo.java

---
+++ VariablesDemo.java
@@ -0,0 +1,16 @@
+public class VariablesDemo {
+    public static void main(String[] args)
+    {
+        int age = 22;
+        double height = 2.75;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name ="Linus";
+
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + age);
+        System.out.println("Height:" + height);
+        System.out.println("Grade:" + grade);
+        System.out.println("Is Student:" + isStudent);
+    }
+}
